2edf2f1641dbf1ab16410a3ba7a73b6b6ac434a4
richelbilderbeek <richel@richelbilderbeek.nl>
2014-11-16 19:10:18 +0100

    FreeMind mindmap and essay title

---
+++ EssayGenomeEvolution/EssayGenomeEvolution.md
@@ -0,0 +1,3 @@
+# What are my insights into bacterial genome evolution?
+
+##
---
+++ EssayGenomeEvolution/GenomeEvolution.mm
@@ -0,0 +1,112 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Genome evolution" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1416159394530"><hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="5"/>
+<node TEXT="Bacteria" POSITION="right" ID="ID_981255768" CREATED="1416159401843" MODIFIED="1416159405123">
+<edge COLOR="#ff0000"/>
+<node TEXT="Computer" ID="ID_840687839" CREATED="1416159888777" MODIFIED="1416159892044">
+<node TEXT="DNA = program" ID="ID_1041755337" CREATED="1416159893033" MODIFIED="1416159896272"/>
+<node TEXT="non-DNA = OS" ID="ID_601601809" CREATED="1416159897171" MODIFIED="1416159904025"/>
+</node>
+</node>
+<node TEXT="Horizontal gene transfer" POSITION="left" ID="ID_1145602355" CREATED="1416159415216" MODIFIED="1416159420005">
+<edge COLOR="#0000ff"/>
+</node>
+<node TEXT="Lateral gene transfer" POSITION="right" ID="ID_598310040" CREATED="1416159421544" MODIFIED="1416159427706">
+<edge COLOR="#00ff00"/>
+</node>
+<node TEXT="Genomics" POSITION="left" ID="ID_1634347872" CREATED="1416159464234" MODIFIED="1416159465977">
+<edge COLOR="#ff00ff"/>
+</node>
+<node TEXT="Genome" POSITION="right" ID="ID_435105317" CREATED="1416159476727" MODIFIED="1416159479747">
+<edge COLOR="#00ffff"/>
+<node TEXT="Architecture" ID="ID_1481112146" CREATED="1416159480956" MODIFIED="1416159483211">
+<node TEXT="One origin" ID="ID_167170817" CREATED="1416159487049" MODIFIED="1416159489609">
+<node TEXT="Higher G+C content" ID="ID_1353567366" CREATED="1416159551580" MODIFIED="1416159559798"/>
+<node TEXT="Lower evolutionary rate" ID="ID_1362195418" CREATED="1416159561342" MODIFIED="1416159577747"/>
+</node>
+<node TEXT="One terminus" ID="ID_500368821" CREATED="1416159491352" MODIFIED="1416159493880"/>
+<node TEXT="Vital genes prefer leading strand" ID="ID_636933256" CREATED="1416159497716" MODIFIED="1416159539215"/>
+<node TEXT="Preference for A+T" ID="ID_1497959590" CREATED="1416159544931" MODIFIED="1416159615995"/>
+<node TEXT="Size" ID="ID_1816236866" CREATED="1416159785160" MODIFIED="1416159787551">
+<node TEXT="Small" ID="ID_1268436120" CREATED="1416159788058" MODIFIED="1416159789462">
+<node TEXT="Good" ID="ID_1682464692" CREATED="1416159793203" MODIFIED="1416159800055"/>
+</node>
+<node TEXT="Big" ID="ID_845534784" CREATED="1416159790152" MODIFIED="1416159791493">
+<node TEXT="Prevents insertions" ID="ID_1925098557" CREATED="1416159805336" MODIFIED="1416159809898"/>
+</node>
+</node>
+<node TEXT="Constraints" ID="ID_779811102" CREATED="1416159937011" MODIFIED="1416159940389">
+<node TEXT="Supercoiling" ID="ID_1614460624" CREATED="1416159941029" MODIFIED="1416159944316"/>
+<node TEXT="Domain structure" ID="ID_1932627265" CREATED="1416159945081" MODIFIED="1416159950470"/>
+<node TEXT="Attachment to specific sites" ID="ID_1253641139" CREATED="1416159951271" MODIFIED="1416159962134"/>
+<node TEXT="Fuzzy motifs" ID="ID_748442884" CREATED="1416159967293" MODIFIED="1416159972639"/>
+</node>
+</node>
+<node TEXT="Core genome" ID="ID_1377745159" CREATED="1416159623207" MODIFIED="1416159628170"/>
+<node TEXT="Pan genome" ID="ID_1798957329" CREATED="1416159629529" MODIFIED="1416159632227">
+<node TEXT="Open" ID="ID_324983412" CREATED="1416159649943" MODIFIED="1416159652564"/>
+<node TEXT="Closed" ID="ID_1253943591" CREATED="1416159656245" MODIFIED="1416159657837"/>
+</node>
+<node TEXT="Genes" ID="ID_629816671" CREATED="1416159670821" MODIFIED="1416159672704">
+<node TEXT="Gain" ID="ID_1234245146" CREATED="1416159674011" MODIFIED="1416159728523">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="80" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1145602355" STARTINCLINATION="525;0;" ENDINCLINATION="525;0;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<node TEXT="Duplicatation" ID="ID_1820487876" CREATED="1416159749380" MODIFIED="1416159753668"/>
+</node>
+<node TEXT="Loss" ID="ID_1982031659" CREATED="1416159736315" MODIFIED="1416159737617">
+<node TEXT="Pseudogene" ID="ID_541334169" CREATED="1416159762308" MODIFIED="1416159764676"/>
+<node TEXT="Gene excision" ID="ID_860732978" CREATED="1416159765549" MODIFIED="1416159770675"/>
+</node>
+</node>
+<node TEXT="Phylogeny" ID="ID_1336298665" CREATED="1416159837507" MODIFIED="1416159840123"/>
+</node>
+</node>
+</map>
